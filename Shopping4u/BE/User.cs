dd2c4509297d5c3c050e05e0259da1dcb4410bf8
MoriyaTzubara <m0533151327@gmail.com>
2021-06-02 20:47:28 +0300

    Basic classes

---
+++ Shopping4u/BE/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping4u.BE
+{
+    public class User
+    {
+        public string firstName;
+        public string lastName;
+        public string phoneNumber;
+        public string email;  // It is also the username
+        public string profileImageUrl;
+
+        public string fullName() { return $"{firstName} {lastName}"; }
+    }
+}
