9d61151e8bfd894406a9e77604dda9f8c1c6e069
MoriyaTzubara <m0533151327@gmail.com>
2021-06-02 20:47:28 +0300

    Basic classes

---
+++ Shopping4u/BL/BL.cs
@@ -0,0 +1,15 @@
+﻿using Shopping4u.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping4u.BL
+{
+    public class BL: IBL
+    {
+        private IDAL dal = new DAL.DAL();
+
+    }
+}
