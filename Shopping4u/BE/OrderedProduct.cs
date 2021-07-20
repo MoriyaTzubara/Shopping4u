61ba4c969b2fb0b6805c5eb2da747a811f4abbf8
ratzontalya <ratzontalya11@gmail.com>
2021-07-21 00:15:32 +0300

    DAL LAYER - select and insert queries

---
+++ Shopping4u/BE/OrderedProduct.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class OrderedProduct
+    {
+        public int shoppingListId;
+        public int branchProductId;
+        public double unitPrice;
+        public int quantity;
+    }
+}
---
+++ Shopping4u/BE/ShoppingList.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class ShoppingList
+    {
+        public int id;
+        public int consumerId;
+        public List<OrderedProduct> products;
+        public DateTime date;
+    }
+}
