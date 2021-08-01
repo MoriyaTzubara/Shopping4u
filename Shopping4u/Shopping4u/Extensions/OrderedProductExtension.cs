f727114cb173cf47c675b87a915862a88cf3522f
MoriyaTzubara <m0533151327@gmail.com>
2021-08-01 22:48:51 +0300

    Adding OrderedProductExtension

---
+++ Shopping4u/Shopping4u/Extensions/OrderedProductExtension.cs
@@ -0,0 +1,17 @@
+﻿using BE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping4u.Extensions
+{
+    public static class OrderedProductExtension
+    {
+        public static Branch GetBranch(this OrderedProduct orderedProduct)
+        {
+            return null;
+        }
+    }
+}
--- Shopping4u/Shopping4u/ViewModels/ProductViewModel.cs
+++ Shopping4u/Shopping4u/ViewModels/ProductViewModel.cs
@@ -11,18 +11,29 @@ namespace Shopping4u.ViewModels
 {
     public class ProductViewModel
     {
-        public OrderedProduct Product { get; set; }
+        public OrderedProduct orderedProduct { get; set; }
 
         public ProductViewModel(OrderedProduct orderedProduct)
         {
-            Product = orderedProduct;
+            this.orderedProduct = orderedProduct;
         }
 
-        public String BranchName { get; set; }
+        public String BranchName { 
+            get { 
+                IBL bl = new BL.BL();
+                return bl.GetBranchName(orderedProduct.branchProductId);
+            }
+            private set { } }
 
-        public String ProductName { get; set; }
+        public String ProductName {
+            get {
+                IBL bl = new BL.BL();
+                return "";
+                //return bl.GetProductName(Product.);
+            }
+            set { } }
         
-        public int Quantity { get; set; }
+        public int Quantity { get { return orderedProduct.quantity; } set { orderedProduct.quantity = value; } }
         
     }
 }
